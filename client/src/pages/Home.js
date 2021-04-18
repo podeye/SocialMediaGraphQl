26f948f687d0225b0b907abc3739338aa75173c3
Andrew Graf <podeye@hotmail.com>
2021-04-18 13:33:31 -0600

    able to get graphql data

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,35 @@
 import React from 'react'
+import {useQuery} from '@apollo/client';
+import gql from 'graphql-tag';
 
 const Home = () => {
+  const { loading, data: { getPosts: posts } = {}} = useQuery(FETCH_POSTS_QUERY)
+  if(posts){
+    console.log('DATA',posts)
+  }
   return (
     <div>
       <h1>Home</h1>
     </div>
   )
 }
+
+const FETCH_POSTS_QUERY = gql`
+{
+    getPosts{
+     id body createdAt username likeCount
+     likes{
+       username
+     }
+     commentCount
+     comments{
+       id
+       username
+       createdAt
+       body
+     }
+    }
+}
+`
 
 export default Home
