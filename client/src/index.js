f672bf6bb9a750d356b2ad8191ffe7ed6ec0b742
Andrew Graf <podeye@hotmail.com>
2021-04-19 11:27:55 -0600

    add posts

--- client/src/index.js
+++ client/src/index.js
@@ -5,15 +5,26 @@ import {InMemoryCache,
         createHttpLink,
         ApolloProvider,
         ApolloClient} from '@apollo/client';
+import {setContext} from 'apollo-link-context';
 
 const httpLink = createHttpLink({
   uri: 'http://localhost:5000'
 })
 
+const authLink = setContext(()=>{
+  const token = localStorage.getItem("jwtToken");
+  return{
+    headers:{
+      Authorization: token?`Bearer ${token}`: ''
+    }
+  }
+})
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 })
+
 
 ReactDOM.render(
   <ApolloProvider client={client}>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import {useQuery} from '@apollo/client';
-import gql from 'graphql-tag';
 import { Grid } from 'semantic-ui-react';
 import PostCard from '../components/PostCard';
+import {AuthContext} from '../context/auth';
+import PostForm from '../components/PostForm';
+import {FETCH_POSTS_QUERY} from '../util/graphql';
+
 
 
 const Home = () => {
+
+  const {user} = useContext(AuthContext);
+
   const { loading, data: { getPosts: posts } = {}} = useQuery(FETCH_POSTS_QUERY)
   return (
     <Grid columns={3}>
@@ -13,6 +19,11 @@ const Home = () => {
          <h1>Recent Posts</h1>
     </Grid.Row>
     <Grid.Row>
+      {user&&(
+        <Grid.Column>
+          <PostForm/>
+        </Grid.Column>
+      )}
       {loading ? (
         <h1>Loading Posts ... </h1>
       ):(
@@ -29,22 +40,5 @@ const Home = () => {
   )
 }
 
-const FETCH_POSTS_QUERY = gql`
-{
-    getPosts{
-     id body createdAt username likeCount
-     likes{
-       username
-     }
-     commentCount
-     comments{
-       id
-       username
-       createdAt
-       body
-     }
-    }
-}
-`
 
 export default Home
